c7495f019d45d25a7e70c3c1eec0f3a6fc47d763
iron8kid <KOranflash101*>
2021-12-03 16:29:45 +0100

    Comportement Gregaire

--- Comportements/Gregaire.cpp
+++ Comportements/Gregaire.cpp
@@ -1,20 +1,20 @@
 #include "Gregaire.h"
 
-double Gregaire::meanOrientation(Milieu & monMilieu, Bestiole & current)
+double Gregaire::meanOrientation(const Milieu & monMilieu, const Bestiole & b)
 {
     double orientation;
     int nb=0;
 
     for ( std::vector<shared_ptr<Bestiole>>::iterator it = monMilieu.getBestiolesList().begin() ; it != monMilieu.getBestiolesList().end() ; ++it ){
-        if ((**it).getID() != current->getID()){
-            if(current->jeTeVois(**it)){
-                orientation+= (**it).getOrientation();
+        if ((*it).getID() != b.getID()){
+            if(b.jeTeVois(*it)){
+                orientation+= (*it).getOrientation();
                 nb += 1;
             }
         }
     }
     if(nb==0){
-         return current->getOrientation();
+         return b->getOrientation();
     }else{
         return orientation/nb;
     }
