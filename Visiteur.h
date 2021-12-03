8bfb6a3e646346c3c266528d5cdab44b3bf3dec2
Mateo Bentura <mateo.bentura-larregui@imt-atlantique.net>
2021-12-03 14:15:15 +0100

    Visitor headers

---
+++ Visiteur.h
@@ -0,0 +1,15 @@
+#ifndef _VISITEUR_
+#define _VISITEUR_
+
+#include <iostream>
+
+class Visitor {
+public:
+   void visitPareseuse(c Pareseuse, *b Bestiole);
+   void visitGregaire(c Gregaire, *b Bestiole);
+   void visitKamikaze(c Kamikaze, *b Bestiole);
+   void visitPrevoyante(c Gregaire, *b Bestiole);
+};
+
+
+#endif
---
+++ VisiteurDeplacement.h
@@ -0,0 +1,14 @@
+#ifndef _VISITEUR_DEPLACEMENT_
+#define _VISITEUR_DEPLACEMENT_
+
+#include "Visiteur.h"
+
+class VisiteurDeplacement: public Visiteur {
+   void visitPareseuse(c Pareseuse, *b Bestiole);
+   void visitGregaire(c Gregaire, *b Bestiole);
+   void visitKamikaze(c Kamikaze, *b Bestiole);
+   void visitPrevoyante(c Gregaire, *b Bestiole);
+}
+
+
+#endif
