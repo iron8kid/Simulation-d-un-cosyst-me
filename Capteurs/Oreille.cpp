7ffa98b2f9d39e9dbe17abd851c52af7e295ac48
Mateo Bentura <mateo.bentura-larregui@imt-atlantique.net>
2021-12-15 23:48:19 +0100

    Ear sensor .cpp file

---
+++ Capteurs/Oreille.cpp
@@ -0,0 +1,35 @@
+#include "Oreille.h"
+#include <iostream>
+#include <random>
+
+const double	Oreille::MAX_DISTANCE = 100.;
+const double	Oreille::MAX_DETECTION = 1.;
+const double	Oreille::MIN_DISTANCE = 0.;
+const double	Oreille::MIN_DETECTION = 0.;
+
+Oreille::Oreille()
+{
+	distance = MIN_DISTANCE + ((double) rand()) / RAND_MAX * MAX_DISTANCE;
+	detection = MIN_DETECTION + ((double) rand()) / RAND_MAX * MAX_DETECTION;
+}
+
+double Oreille::getDistance ( void ) const
+{
+	return distance;
+}
+
+bool Oreille::detecte( double distance_autre ) const
+{
+	bool dist_cond = distance_autre < distance;
+	return (dist_cond);
+}
+
+Oreille& Oreille::operator=( const Oreille& other )
+{
+   // Guard self assignment
+    if (this == &other)
+        return *this;
+    distance = other.distance;
+    detection = other.detection;
+	return *this;
+}
